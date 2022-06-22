c05c4034f1bfccfda272ea269decb84447a9cdb0
Ronit <ronitrks6666@gmail.com>
2022-06-23 00:58:50 +0530

    Minor deploy changes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const notes = require('./models/notesModel')
 require('dotenv').config()
-
+const  path = require('path')
 
 
 
@@ -15,10 +15,16 @@ const notesRoute = require('./routes/NotesRoute')
 app.use('/api',notesRoute)
 
 
-
+const __dirname1 = path.resolve();
+if(process.env.NODE_ENV==='production'){
+    app.use(express.static(path.join(__dirname1,'client/build')));
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname1,"client","build","index.html"))
+    })
+}
 
  
  
-app.listen(8000,(req,res)=>{
+app.listen(port,(req,res)=>{
     console.log("Server is running :]")
 })
